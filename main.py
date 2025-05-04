526f3f7406d7137dd36f86c20fcde814ac8ea106
JoaoPauloLordelo <joao.lordelo@souunit.com.br>
2025-05-04 17:19:08 -0300

    atualização de main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+from fastapi import FastAPI
+import psycopg2
+
+#uvicorn main:app --host 127.0.0.1 --port 8000
+
+app = FastAPI()
+
+conn = psycopg2.connect(
+    dbname="teste",
+    user="postgres",
+    password="JPV0510!",
+    host="localhost",
+    port="5432"
+)
+
+@app.get("/")
+def hello():
+    cur = conn.cursor()
+    cur.execute("SELECT descricao FROM cs_acoes;")
+    msg = cur.fetchall()
+    cur.close()
+@app.post("/")
+def enviar():
+    return {}
