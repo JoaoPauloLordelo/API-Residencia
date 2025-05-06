a1d0b95c47f47424556a26345d860738777b2aae
Abraao <abraao.neto@souunit.com.br>
2025-05-06 16:22:39 -0300

    Principal funcao na home, pega a ultima acao e descricao

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
-conexao = psycopg2.connect(database = "sq07_db", host = "191.195.32.239", user = "squad07", password = "itpSquad07" , port = "5432")
+conexao = psycopg2.connect(database = "sq07_db", host = "localhost", user = "postgres", password = "45987956aA@" , port = "5432")
 
 print(conexao.status) #checkando conexão, se retornar 1 deu certo 
 
-@app.get("/") #rota, local onde deve ser colocado no sitee
+@app.get("/") 
 
 def home():
-    return " Api online"
-
-@app.get("/acoes")
-def get_all_acoes():
-
-    #Retorna todas as ações, com acao_id e descricao.
+ # Retorna útlima acao e descrição
     try:
         with conexao.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT acao_id, descricao FROM cs_acoes;")
-            resultados = cur.fetchall()
+            cur.execute("SELECT acao_id, descricao FROM cs_acoes ORDER BY acao_id DESC LIMIT 1;")
+            resultados = cur.fetchone()
+        return resultados
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) 
+    
+@app.get("/acoes")
+
+def get_all_acoes():
+    #Retorna todas as ações, com acao_id e descricao.
+    try:
+        cur = conexao.cursor()
+        cur.execute("SELECT acao_id, descricao FROM cs_acoes;")
+        resultados = cur.fetchall()
         return resultados
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 
 @app.get("/acoes/{acao_id}")
 def get_acao_by_id(acao_id: int):
 
     #Retorna uma única ação pelo seu acao_id.
- 
     try:
         with conexao.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(
