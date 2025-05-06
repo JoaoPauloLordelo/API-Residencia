b63016fb25fa92df7fdf22d4c525fcf4ece59085
arthuroliveira06 <arthur.oliveira06@souunit.com.br>
2025-05-06 10:33:03 -0300

    Mudanças no hook

--- hook.py
+++ hook.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import event, update
+import psycopg2
+from sqlalchemy.engine import Connection
+from psycopg2.extras import RealDictCursor
+conexao = psycopg2.connect(database = "sq07_db", host = "191.195.32.239", user = "squad07", password = "itpSquad07" , port = "5432")
+
+def get_acao_by_id(acao_id: int):
+    # Retorna uma única ação pelo seu acao_id.
+
+    try:
+        with conexao.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                "SELECT acao_id, descricao FROM cs_acoes WHERE acao_id = %s;",
+                (acao_id,)
+            )
+            resultado = cur.fetchone()
+        if not resultado:
+            raise HTTPException(status_code=404, detail="Ação não encontrada")
+        return resultado
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 @event.listens_for(cs_actions, "after_insert")
-#after_insert_hook(mapper, connection: Connection, target):
-    #try:
-        #resultado = analisar_mensagem(target.descricao)
-
-        #stmt = (
-            #update(CsActions)
-            #.where(CsActions.acao_id == target.acao_id)
-            #.values(sentimento=resultado)
-       # )
-        #connection.execute(stmt)
-
-   # except Exception as e:
-       # print(f"Erro no hook: {str(e)}")
+def  after_insert_hook(mapper, connection: Connection, target):
+    acao_id = target.acao_id
+    try:
+        acao = get_acao_by_id(acao_id)
+        print("Ação inserida:", acao)
+    except HTTPException as e:
+        print("Erro ao buscar ação após insert:", str(e.detail))
 
 #obs: função não será utilizada, esperando um metodo get pra modificar, cs_actions é referente ao modelo no banco postgres, nome pode ser mudado.
+##mapper: mapeador do sqlalchemy, nesse caso pegaria o parametro cs_actions
+##connection, conexão com o banco de dados
+##target: instancia da classe do modelo
