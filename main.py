2f597f0816542e24b6209a00e08f76f907643216
João Paulo <joao.lordelo@souunit.com.br>
2025-05-15 21:15:05 -0300

    Atualização da main, junção das funções, e do gitignore

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import event, update
 import psycopg2
-from sqlalchemy.engine import Connection
 from medolo import analisadorSentimento
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Text, ForeignKey,String,TIMESTAMP
 #uvicorn main:app --host 127.0.0.1 --port 8000
 
 app = FastAPI()
 
 conn = psycopg2.connect(
-    dbname="",
-    user="",
-    password="",
-    host="",
-    port=""
+    dbname="teste02",
+    user="postgres",
+    password="JPV0510!",
+    host="localhost",
+    port="5432"
 )
 
 def salvar_sentimento(sentimento, acao_id):
@@ -26,47 +22,19 @@ def salvar_sentimento(sentimento, acao_id):
         """, (sentimento, acao_id))
         conn.commit()
         cur.close()
-        conn.close()
         return "Deu certo"
     except Exception:
         return "[ERRO AO INSERIR NO BANCO]"
     
-def verificar_funcoes():
-    cur = conn.cursor()
-
-    cur.execute("SELECT 1 FROM pg_proc WHERE proname = 'notify_cs_acoes';")
-    func_exists = cur.fetchone() is not None
-
-    cur.execute("""
-        SELECT 1
-        FROM information_schema.triggers
-        WHERE event_object_table = 'cs_acoes'
-          AND trigger_name = 'trg_cs_acoes_notify';
-    """)
-    trigger_exists = cur.fetchone() is not None
-    if func_exists and trigger_exists == True:
-        return True
-    else:
-        return False
-    
-def criarTrigger():
-    cur = conn.cursor()
-
 @app.get("/")
 def  after_insert_hook():
     try:
-        # cur = conn.cursor()
-        # cur.execute("SELECT descricao,acao_id FROM cs_acoes ORDER BY acao_id DESC LIMIT 1;")
-        # msg = cur.fetchall()
-        # cur.close()
-        # # r = analisadorSentimento(msg) #--Analisa o sentimento ( Temporariamente desativado para deixar mais leve)
-        # salvar_sentimento(msg[0],msg[1])
-        # return True
-        if verificar_funcoes() == True:
-            return "Deu merda"
-        else:
-            return "Deu bom"
-    
+        cur = conn.cursor()
+        cur.execute("SELECT descricao,acao_id FROM cs_acoes ORDER BY acao_id DESC LIMIT 1;")
+        msg = cur.fetchone()
+        # r = analisadorSentimento(msg) #--Analisa o sentimento ( Temporariamente desativado para deixar mais leve)
+        cur.close()
+        return salvar_sentimento(msg[0],msg[1])
     except HTTPException:
          return "Erro ao conectar com o banco"
  
